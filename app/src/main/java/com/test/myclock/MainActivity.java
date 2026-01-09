a463e6ee460301f19d1f9a501cf2e8ebb9ab6c72
anirudhchakraborty <preetiujjal@gmail.com>
2026-01-09 08:37:39 +0530

    feat: Screen Complete

--- app/src/main/java/com/test/myclock/MainActivity.java
+++ app/src/main/java/com/test/myclock/MainActivity.java
@@ -1,6 +1,8 @@
 package com.test.myclock;
 
 import android.os.Bundle;
+import android.widget.TextClock;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    TimePicker alarmTime;
+    TextClock currentTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        alarmTime = findViewById(R.id.timePicker);
+        currentTime = findViewById(R.id.textClock);
+
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+
+                if(currentTime.getText().toString().equals(AlarmTime())){
+
+                }
+            }
+        },0,1000);
+    }
+
+
+    public String AlarmTime(){
+
+        Integer alarmHours = alarmTime.getHour();
+        Integer alarmMinutes = alarmTime.getMinute();
+
+        return "helo";
     }
 }
